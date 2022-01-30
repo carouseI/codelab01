697feb67bfd8aad6d695d6806e70827ff4d38d20
headlesscrown <mdh8533@nyu.edu>
2022-01-29 23:57:54 -0500

    movement code edit

--- cl01_game01/Assets/PlayerMovement.cs
+++ cl01_game01/Assets/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    private float dirX = 0f;
+
     //int wholeNumber = 16; //store whole number in variable
     //float decimalNumber = 4.56f; //floating point literal
     //string text = "blank"; //entry text
@@ -24,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
     {
         //Debug.Log("Hello, world!");
 
-        float dirX = Input.GetAxisRaw("Horizontal"); //left: -1, right: +1
+        dirX = Input.GetAxisRaw("Horizontal"); //left: -1, right: +1
         rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
 
 
@@ -33,11 +35,16 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, 14f); //call rigidbody + add speed
         }
 
-        if(dirX > 0f)
+        UpdateAnimationUpdate(); //call method
+    }
+
+    private void UpdateAnimationUpdate() //no results returned, exclusive execution
+    {
+        if (dirX > 0f)
         {
             anim.SetBool("running", true);
         }
-        else if(dirX < 0)
+        else if (dirX < 0)
         {
             anim.SetBool("running", true); //running to the left
         }
