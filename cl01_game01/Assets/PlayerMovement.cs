20e776a0f40788fb6588d03372c7443de066b95f
headlesscrown <mdh8533@nyu.edu>
2022-01-29 23:18:39 -0500

    jump code update

--- cl01_game01/Assets/PlayerMovement.cs
+++ cl01_game01/Assets/PlayerMovement.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private Rigidbody2D rb;
 
+    //int wholeNumber = 16; //store whole number in variable
+    //float decimalNumber = 4.56f; //floating point literal
+    //string text = "blank"; //entry text
+    //bool boolean = false; //check true/false conditions, executes based on condition
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         //Debug.Log("Hello, world!");
-        if (Input.GetKeyDown("space"))
+        if (Input.GetButtonDown("Jump")) //GBD vs GKD, uses input system
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, 14, 0); //call rigidbody + add speed
+            rb.velocity = new Vector3(0, 14f, 0); //call rigidbody + add speed
         }
     }
 }
