ad54069269ec7d96860113e04b976c6540db3cc2
headlesscrown <mdh8533@nyu.edu>
2022-01-30 22:34:19 -0500

    linked start scene with level 01

--- cl01_game01/Assets/Scripts/StartMenu.cs
+++ cl01_game01/Assets/Scripts/StartMenu.cs
@@ -5,5 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //load level 01
+    }
 }
