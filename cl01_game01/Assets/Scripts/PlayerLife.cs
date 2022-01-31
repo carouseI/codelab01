a818f206b069a4b50aaf359177aa1f9849c72b81
headlesscrown <mdh8533@nyu.edu>
2022-01-30 21:55:07 -0500

    player death animation update
    restart  level script mod

--- cl01_game01/Assets/Scripts/PlayerLife.cs
+++ cl01_game01/Assets/Scripts/PlayerLife.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
-
+    private Rigidbody2D rb;
     private Animator anim;
 
     // Start is called before the first frame update
     private void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
@@ -23,7 +25,12 @@ public class PlayerLife : MonoBehaviour
 
     private void Die()
     {
+        rb.bodyType = RigidbodyType2D.Static; //set player to static 
         anim.SetTrigger("death"); //execute death animation
     }
 
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //reload level
+    }
 }
