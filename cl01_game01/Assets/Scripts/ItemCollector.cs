bddc3613d7123cecc8793c77768f46d6ac999816
headlesscrown <mdh8533@nyu.edu>
2022-01-30 21:30:09 -0500

    item collector script update

--- cl01_game01/Assets/Scripts/ItemCollector.cs
+++ cl01_game01/Assets/Scripts/ItemCollector.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
 {
     private int melons = 0; //counter
+
+    [SerializeField] private Text melonText;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,7 +15,8 @@ public class ItemCollector : MonoBehaviour
         {
             Destroy(collision.gameObject); //remove from game
             melons++; //increment value
-            Debug.Log("Melons: " + melons); //add number to string, show numbre behind
+            //Debug.Log("Melons: " + melons); //add number to string, show numbre behind
+            melonText.text = "Melons: " + melons;
         }
     }
 }
