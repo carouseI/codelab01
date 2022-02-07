bf19f1c0ccf7ad835df836b6385e162f28631ef8
headlesscrown <mdh8533@nyu.edu>
2022-02-06 22:23:18 -0500

    game manager mod

--- cl01_game01/Assets/Scripts/GameManager.cs
+++ cl01_game01/Assets/Scripts/GameManager.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance; //static, shared among instances, no reference needed
+
+    public bool HitCheckpoint { get; set; } //auto public bool property
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance is null) //check if null
+                Debug.LogError("Game Manager is NULL");
+
+            return _instance; //if null
+        }
+
+        private void Awake()
+    {
+        _instance = this; //initialise private instance
+    }
+}
     // Start is called before the first frame update
     void Start()
     {
