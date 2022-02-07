c451ea3a66f9e9569b763addb0e98fe771f91c67
headlesscrown <mdh8533@nyu.edu>
2022-02-06 22:09:47 -0500

    new game manager

--- cl01_game01/Assets/Scripts/GameManager.cs
+++ cl01_game01/Assets/Scripts/GameManager.cs
@@ -1,13 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public static float score; //static, accessible by other scripts, no reference to instance needed
-
-    public const float GRAVITY = 
     // Start is called before the first frame update
     void Start()
     {
