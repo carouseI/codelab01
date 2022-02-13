2c2561b215b6fa0d71de2751e75196d118b0c4bd
headlesscrown <mdh8533@nyu.edu>
2022-02-13 10:31:44 -0500

    add high score mod

--- cl01_game01/Assets/Scripts/Melon.cs
+++ cl01_game01/Assets/Scripts/Melon.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Melon : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Text score; //accessible across project
+    public Text highScore;
+
     void Start()
     {
-        
+        highScore.text = PlayerPrefs.GetInt("HighScore", 0).ToString(); //store number as high score, display next time game is restarted 
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateScore()
     {
-        
+        int number = Random.Range(1, 10); //int = store in variables
+        score.text = number.ToString(); //ToString = convert to string to add up/match with score
+
+        PlayerPrefs.SetInt("HighScore", number);
     }
 }
