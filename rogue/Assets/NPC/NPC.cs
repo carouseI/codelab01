04525d04fe46a8f76f23fa74c9a0e55d91b6f499
headlesscrown <mdh8533@nyu.edu>
2022-03-05 15:13:39 -0500

    npc script new

---
+++ rogue/Assets/NPC/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.NPC
+{
+    public class NPC: MonoBehaviour
+    {
+
+        public void Awake()
+        {
+
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
