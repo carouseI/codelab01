bc3fa9d41f06ec19a3d23bd42bba6fe257f4db31
headlesscrown <mdh8533@nyu.edu>
2022-03-05 15:53:40 -0500

    update

--- rogue/Assets/NPC/NPCPatrolPoint.cs
+++ rogue/Assets/NPC/NPCPatrolPoint.cs
@@ -16,10 +16,25 @@ namespace Assets.Code.NPC
 
         List<NPCPatrolPoint> _connections;
 
-        // Start is called before the first frame update
+
         public void Start()
         {
             GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("PatrolPoint"); //get all waypoint objects in scene
+
+            _connections = new List<NPCPatrolPoint>(); //create waypoint reference list
+
+            for(int i = 0; i < allWaypoints.Length; i++) //check for waypoint connection
+            {
+                NPCPatrolPoint nextWaypoint = allWaypoints[i].GetComponent<NPCPatrolPoint>();
+
+                if(nextWaypoint != null) //if not null
+                {
+                    if(Vector3.Distance(this.transform.position, nextWaypoint.transform.position) <= _connectivityRadius && nextWaypoint)
+                    {
+                        _connections.Add(nextWaypoint);
+                    }
+                }
+            }
         }
 
         // Update is called once per frame
