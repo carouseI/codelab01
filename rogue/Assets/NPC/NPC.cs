96c2c222a9e322f359434faf65e93b60c8727ab9
headlesscrown <mdh8533@nyu.edu>
2022-03-05 15:21:56 -0500

    update

--- rogue/Assets/NPC/NPC.cs
+++ rogue/Assets/NPC/NPC.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.AI; //permission to access nav mesh agent
 
 namespace Assets.Code.NPC
 {
+    [RequireComponent(typeof(NavMeshAgent))] //embedded rule; when instance is created, must have a nav mesh agent + finite state machine attached to same game object -- if not, add missing components
+
     public class NPC: MonoBehaviour
     {
+        NavMeshAgent _navMeshAgent;
 
         public void Awake()
         {
-
+            _navMeshAgent = this.GetComponent<NavMeshAgent>(); //this. = optional, gets autocorrect to kick in
         }
 
         public void Start()
