47f89803c66a3f77bdd55baa1e6f61f0c9dff82b
headlesscrown <mdh8533@nyu.edu>
2022-03-05 14:27:43 -0500

    abstract fms state script add

--- rogue/Assets/FSM/AbstractFSMState.cs
+++ rogue/Assets/FSM/AbstractFSMState.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AbstractFSMState : MonoBehaviour
+public enum ExecutionState //check if states are working + done executing
 {
-    // Start is called before the first frame update
-    void Start()
+    NONE,
+    ACTIVE,
+    COMPLETED,
+    TERMINATED,
+};
+
+public abstract class AbstractFSMState : ScriptableObject //doesn't need to be attached to game object to function
+{
+
+    public ExecutionState ExecutionState { get; protected set; } //in-line property, only state can change execution state of state is
+
+    public virtual void OnEnable()
     {
-        
+        ExecutionState = ExecutionState.NONE; //state not yet active, req state machine to set up and activate
     }
 
-    // Update is called once per frame
-    void Update()
+    //virtual = by default, any subclasses = can override
+    public virtual bool EnterState() //enter state successfully without errors
     {
-        
+        ExecutionState = ExecutionState.ACTIVE;
+        return true;
     }
+
+    public abstract void UpdateState(); //in finite state machine, update current state active in machine; can restrain, monobehaviour = no restraint on tic
+    //abstract = new state class, must write update method, can override existing enter/exit states
+
+    public virtual bool ExitState() //exit state with proper configuration
+    {
+        ExecutionState = ExecutionState.COMPLETED;
+        return true;
+    }
+    
 }
