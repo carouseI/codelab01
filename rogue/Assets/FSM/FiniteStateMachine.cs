e584467b783baf78066f1d08baeacec563996c5c
headlesscrown <mdh8533@nyu.edu>
2022-03-05 15:40:34 -0500

    idle state add

---
+++ rogue/Assets/FSM/FiniteStateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.FSM
+{
+    public class FiniteStateMachine: MonoBehaviour
+    {
+        [SerializeField] //expose in editor
+        AbstractFSMState _startingState; //variable
+
+        AbstractFSMState _currentState;
+
+        public void Awake()
+        {
+            _currentState = null; 
+        }
+
+        public void Start()
+        {
+            if(_startingState != null) //if starting state is not null, enter state
+            {
+                EnterState(_startingState);
+            }
+        }
+
+        public void Update()
+        {
+            if(_currentState != null) //if not null, update current state
+            {
+                _currentState.UpdateState();
+            }
+        }
+
+        #region STATE MANAGEMENT
+
+        public void EnterState(AbstractFSMState nextState) //pass in state of FSM
+        {
+            if(nextState == null) //if null, pull out of method
+            {
+                return;
+            }
+
+            _currentState = nextState;
+            _currentState.EnterState();
+        }
+
+        #endregion
+    }
+}
--- rogue/Assets/NPC/NPC.cs
+++ rogue/Assets/NPC/NPC.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Assets.Code.FSM;
 using UnityEngine;
 using UnityEngine.AI; //permission to access nav mesh agent
 
 namespace Assets.Code.NPC
 {
-    [RequireComponent(typeof(NavMeshAgent))] //embedded rule; when instance is created, must have a nav mesh agent + finite state machine attached to same game object -- if not, add missing components
+    [RequireComponent(typeof(NavMeshAgent), typeof(FiniteStateMachine))] //embedded rule; when instance is created, must have a nav mesh agent + finite state machine attached to same game object -- if not, add missing components
 
     public class NPC: MonoBehaviour
     {
         NavMeshAgent _navMeshAgent;
+        FiniteStateMachine _finiteStateMachine; //attached instance here + required comps
 
         public void Awake()
         {
             _navMeshAgent = this.GetComponent<NavMeshAgent>(); //this. = optional, gets autocorrect to kick in
+            _finiteStateMachine = this.GetComponent<FiniteStateMachine>();
         }
 
         public void Start()
