4ccd29a2edf7d826600763bc4348a7960a51428b
headlesscrown <mdh8533@nyu.edu>
2022-03-05 14:45:07 -0500

    new class
    idle state

---
+++ rogue/Assets/FSM/States/IdleState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Code.FSM.States
+{
+    public class IdleState: AbstractFSMState
+    {
+        public override bool EnterState() //call base implementation on abstract class
+        {
+            base.EnterState();
+
+            Debug.Log("entered idle state");
+
+            return true;
+        }
+
+        public override void UpdateState() //no error detected with idle state; preventative measure, stops compiler from generating errors*
+        {
+            //throw new NotImplementedException(); //throw exception every fram update
+
+            Debug.Log("updating idle state");
+        }
+
+        public override ExitState()
+        {
+            base.ExitState(); //call parent state
+
+            Debug.Loge("exiting idle state");
+
+            return true;
+        }
+    }
+}
