61059b5980f867f9f5a6f9398eae813e39f14945
headlesscrown <mdh8533@nyu.edu>
2022-03-15 15:00:28 -0400

    create waypoint script

---
+++ rogue/Assets/Scripts/Waypoint.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField]
+    protected float debugDrawRadius = 1.0F;
+
+    public virtual void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; //set gizmo colour
+        Gizmos.DrawWireSphere(transform.position, debugDrawRadius); //has transform cos it inherits from monobehaviour; draw sphere from current position of object
+    }
+}
