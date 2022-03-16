6aa2e327f7a785d97652eb0736c5c27326bab587
headlesscrown <mdh8533@nyu.edu>
2022-03-16 12:59:02 -0400

    update npc connected patrol script

--- rogue/Assets/Scripts/NPCConnectedPatrol.cs
+++ rogue/Assets/Scripts/NPCConnectedPatrol.cs
@@ -13,21 +13,65 @@ namespace Assets.Code
         [SerializeField]
         bool _patrolWaiting;
 
+
+        //total wait time at each node
         [SerializeField]
         float _totalWaitTime = 3f; //
 
+
+        //probability of switching direction
         [SerializeField]
         float _switchProbability = 0.2f;
 
+
+        //private variables for base behaviour
         NavMeshAgent _navMeshAgent;
         ConnectedWaypoint _currentWaypoint;
         ConnectedWaypoint _previousWaypoint;
 
+
         bool _travelling;
+        bool _waiting;
+        float _waitTimer;
+        int _waypointsVisited;
+
 
         // Start is called before the first frame update
-        void Start()
+        public void Start()
         {
+            _navMeshAgent = this.GetComponent<NavMeshAgent>();
+
+            if(_navMeshAgent == null) //if null
+            {
+                Debug.LogError("nav mesh agent comp not attached to " + gameObject.name); //show this msg
+            }
+            else
+            {
+                if(_currentWaypoint == null) //if no waypoint
+                {
+                    GameObject[] allWaypoints = GameObject.FindGameObjectsWithTag("Waypoint"); //find all available waypoint objects in scene
+
+                    if(allWaypoints.Length > 0) //if number of points is greater than 0
+                    {
+                        while(_currentWaypoint == null)
+                        {
+                            int random = UnityEngine.Random.Range(0, allWaypoints.Length); //set at random
+                            ConnectedWaypoint startingWaypoint = allWaypoints[random].GetComponent<ConnectedWaypoint>(); //check if waypoint comp is attached
+
+                            if(startingWaypoint != null) //if not null
+                            {
+                                _currentWaypoint = startingWaypoint; //set to current waypoint
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Debug.LogError("failed to locate usable waypoints in scene"); //show this msg
+                    }
+                }
+
+                SetDestination();
+            }
 
         }
 
