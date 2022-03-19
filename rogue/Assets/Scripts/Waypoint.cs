a4b992d00695c15b97ddc6dc03f9039104d719c6
headlesscrown <mdh8533@nyu.edu>
2022-03-19 15:37:14 -0400

    waypoint script update
    line 9 -- remove empty line

--- rogue/Assets/Scripts/Waypoint.cs
+++ rogue/Assets/Scripts/Waypoint.cs
@@ -7,7 +7,6 @@ public class Waypoint : MonoBehaviour
     [SerializeField]
     protected float debugDrawRadius = 1.0F;
 
-
     public virtual void OnDrawGizmos()
     {
         Gizmos.color = Color.red; //set gizmo colour
