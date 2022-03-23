1d97136c70fc51cf2843ba6be6503ed06c0e8615
headlesscrown <mdh8533@nyu.edu>
2022-03-22 23:27:51 -0400

    animator handler script update
    replace v value with h for horizontal sect

--- dashi/Assets/Scripts/Player/AnimatorHandler.cs
+++ dashi/Assets/Scripts/Player/AnimatorHandler.cs
@@ -49,27 +49,27 @@ namespace Dashi
 
             #region Horizontal
             //clamp horizontal movement
-            float v = 0;
+            float h = 0;
 
             if (horizontalMovement > 0 && horizontalMovement < 0.55f) // if greater than 0 + less than 0.55
             {
-                v = 0.5f;
+                h = 0.5f;
             }
             else if (horizontalMovement > 0.55f) //if greater than 0.55
             {
-                v = 1;
+                h = 1;
             }
             else if (horizontalMovement < 0 && horizontalMovement > -0.55f) //if less than 0 + greater tan -0.55
             {
-                v = -0.5f;
+                h = -0.5f;
             }
             else if (horizontalMovement < -0.55f) //if less than -0.55
             {
-                v = -1;
+                h = -1;
             }
             else
             {
-                v = 0;
+                h = 0;
             }
             #endregion
 
