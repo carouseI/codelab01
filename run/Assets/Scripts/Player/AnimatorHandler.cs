6e568d9c941cf3972ddec37fb944fc1d18037eac
headlesscrown <mdh8533@nyu.edu>
2022-03-27 16:09:17 -0400

    dashi copy
    update namespace to run

---
+++ run/Assets/Scripts/Player/AnimatorHandler.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace run
+{
+    public class AnimatorHandler : MonoBehaviour
+    {
+        public Animator anim;
+        int vertical;
+        int horizontal;
+        public bool canRotate;
+
+        public void Initialize()
+        {
+            anim = GetComponent<Animator>();
+            vertical = Animator.StringToHash("Vertial");
+            horizontal = Animator.StringToHash("Horizontal");
+        }
+
+        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+        {
+            #region Vertical
+            //clamp vertical movement
+            float v = 0;
+
+            if(verticalMovement > 0 && verticalMovement < 0.55f) // if greater than 0 + less than 0.55
+            {
+                v = 0.5f;
+            }
+            else if(verticalMovement > 0.55f) //if greater than 0.55
+            {
+                v = 1;
+            }
+            else if(verticalMovement < 0 && verticalMovement > -0.55f) //if less than 0 + greater tan -0.55
+            {
+                v = -0.5f;
+            }
+            else if(verticalMovement < -0.55f) //if less than -0.55
+            {
+                v = -1;
+            }
+            else
+            {
+                v = 0;
+            }
+            #endregion
+
+
+            #region Horizontal
+            //clamp horizontal movement
+            float h = 0;
+
+            if (horizontalMovement > 0 && horizontalMovement < 0.55f) // if greater than 0 + less than 0.55
+            {
+                h = 0.5f;
+            }
+            else if (horizontalMovement > 0.55f) //if greater than 0.55
+            {
+                h = 1;
+            }
+            else if (horizontalMovement < 0 && horizontalMovement > -0.55f) //if less than 0 + greater tan -0.55
+            {
+                h = -0.5f;
+            }
+            else if (horizontalMovement < -0.55f) //if less than -0.55
+            {
+                h = -1;
+            }
+            else
+            {
+                h = 0;
+            }
+            #endregion
+
+
+            anim.SetFloat(vertical, v, 0.1f, Time.deltaTime);
+            anim.SetFloat(horizontal, v, 0.1f, Time.deltaTime);
+        }
+
+        public void CanRotate()
+        {
+            canRotate = true;
+        }
+
+        public void StopRotation()
+        {
+            canRotate = false;
+        }
+    }
+}
---
+++ run/Assets/Scripts/References/NPC/NPC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Code.FSM;
+using UnityEngine;
+using UnityEngine.AI; //permission to access nav mesh agent
+
+namespace Assets.Code.NPCCode
+{
+    //[RequireComponent(typeof(NavMeshAgent), typeof(FiniteStateMachine))] //embedded rule; when instance is created, must have a nav mesh agent + finite state machine attached to same game object -- if not, add missing components; NPC pre-reqs
+
+    public class NPC : MonoBehaviour
+    {
+        [SerializeField]
+        NPCPatrolPoint[] _patrolPoints; //add as serialised field to enable attachments
+
+        NavMeshAgent _navMeshAgent;
+        //FiniteStateMachine _finiteStateMachine; //attached instance here + required comps
+
+        public void Awake()
+        {
+            _navMeshAgent = this.GetComponent<NavMeshAgent>(); //this. = optional, gets autocorrect to kick in
+            FiniteStateMachine _finiteStateMachine = this.GetComponent<FiniteStateMachine>(); //fsm at beginning = generate local, _fsm  underlined red; TEMP
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public NPCPatrolPoint[] PatrolPoints
+        {
+            get
+            {
+                return _patrolPoints; //return
+            }
+        }
+    }
+}
