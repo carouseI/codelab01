d2dfc808d55e21067ff9b2913243db80c905f042
headlesscrown <mdh8533@nyu.edu>
2022-03-27 17:01:40 -0400

    input manager update

--- run/Assets/Scripts/InputManager.cs
+++ run/Assets/Scripts/InputManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    PlayerControls playerControls;
+
+    public Vector2 movementInput; //vector2 = store info on 2 axis; up/down, left/right 
+
+    private void OnEnable()
     {
-        
+        if(playerControls == null) //if null
+        {
+            playerControls = new PlayerControls(); //use new control setup
+
+            playerControls.PlayerMovement.Movement.performed += i => movementInput = i.ReadValue<Vector2>(); //when key is hit, record movement to movementInput variable
+
+        }
+
+        playerControls.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        playerControls.Disable(); //turn off player controls when gameObject is disabled
     }
 }
