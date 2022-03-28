7a294c8cb4c61f266587a5cfc1309cdebfae2c0f
headlesscrown <mdh8533@nyu.edu>
2022-03-28 13:37:03 -0400

    added namespace to animator handler

--- run/Assets/Scripts/Ignore/References/Player/AnimatorHandler.cs
+++ run/Assets/Scripts/Ignore/References/Player/AnimatorHandler.cs
@@ -1,122 +1,122 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-namespace run
-{
-    public class AnimatorHandler : MonoBehaviour
-    {
-        PlayerManager playerManager;
-        //InputHandler inputHandler;
-        PlayerLocomotion playerLocomotion;
-        int vertical;
-        int horizontal;
-        public bool canRotate;
+//namespace Run
+//{
+//    public class AnimatorHandler : MonoBehaviour
+//    {
+//        PlayerManager playerManager;
+//        InputHandler inputHandler;
+//        PlayerLocomotion playerLocomotion;
+//        int vertical;
+//        int horizontal;
+//        public bool canRotate;
 
-        public void Initialize()
-        {
-            playerManager = GetComponentInParent<PlayerManager>();
-            //anim = GetComponent<Animator>();
-            //inputHandler = GetComponentInParent<PlayerLocomotion>();
-            vertical = Animator.StringToHash("Vertial");
-            horizontal = Animator.StringToHash("Horizontal");
-        }
+//        public void Initialize()
+//        {
+//            playerManager = GetComponentInParent<PlayerManager>();
+//            anim = GetComponent<Animator>();
+//            inputHandler = GetComponentInParent<PlayerLocomotion>();
+//            vertical = Animator.StringToHash("Vertial");
+//            horizontal = Animator.StringToHash("Horizontal");
+//        }
 
-        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
-        {
-            #region Vertical
-            //clamp vertical movement
-            float v = 0;
+//        public void UpdateAnimatorValues(float verticalMovement, float horizontalMovement)
+//        {
+//            #region Vertical
+//            //clamp vertical movement
+//            float v = 0;
 
-            if(verticalMovement > 0 && verticalMovement < 0.55f) // if greater than 0 + less than 0.55
-            {
-                v = 0.5f;
-            }
-            else if(verticalMovement > 0.55f) //if greater than 0.55
-            {
-                v = 1;
-            }
-            else if(verticalMovement < 0 && verticalMovement > -0.55f) //if less than 0 + greater tan -0.55
-            {
-                v = -0.5f;
-            }
-            else if(verticalMovement < -0.55f) //if less than -0.55
-            {
-                v = -1;
-            }
-            else
-            {
-                v = 0;
-            }
-            #endregion
+//            if (verticalMovement > 0 && verticalMovement < 0.55f) // if greater than 0 + less than 0.55
+//            {
+//                v = 0.5f;
+//            }
+//            else if (verticalMovement > 0.55f) //if greater than 0.55
+//            {
+//                v = 1;
+//            }
+//            else if (verticalMovement < 0 && verticalMovement > -0.55f) //if less than 0 + greater tan -0.55
+//            {
+//                v = -0.5f;
+//            }
+//            else if (verticalMovement < -0.55f) //if less than -0.55
+//            {
+//                v = -1;
+//            }
+//            else
+//            {
+//                v = 0;
+//            }
+//            #endregion
 
-            #region Horizontal
-            //clamp horizontal movement
-            float h = 0;
+//            #region Horizontal
+//            //clamp horizontal movement
+//            float h = 0;
 
-            if (horizontalMovement > 0 && horizontalMovement < 0.55f) // if greater than 0 + less than 0.55
-            {
-                h = 0.5f;
-            }
-            else if (horizontalMovement > 0.55f) //if greater than 0.55
-            {
-                h = 1;
-            }
-            else if (horizontalMovement < 0 && horizontalMovement > -0.55f) //if less than 0 + greater tan -0.55
-            {
-                h = -0.5f;
-            }
-            else if (horizontalMovement < -0.55f) //if less than -0.55
-            {
-                h = -1;
-            }
-            else
-            {
-                h = 0;
-            }
-            #endregion
+//            if (horizontalMovement > 0 && horizontalMovement < 0.55f) // if greater than 0 + less than 0.55
+//            {
+//                h = 0.5f;
+//            }
+//            else if (horizontalMovement > 0.55f) //if greater than 0.55
+//            {
+//                h = 1;
+//            }
+//            else if (horizontalMovement < 0 && horizontalMovement > -0.55f) //if less than 0 + greater tan -0.55
+//            {
+//                h = -0.5f;
+//            }
+//            else if (horizontalMovement < -0.55f) //if less than -0.55
+//            {
+//                h = -1;
+//            }
+//            else
+//            {
+//                h = 0;
+//            }
+//            #endregion
 
-            //if (isSprinting)
-            //{
-            //    //v = 2;
-            //    //h = horizontalMovement;
-            //}
+//            //if (isSprinting)
+//            //{
+//            //    //v = 2;
+//            //    //h = horizontalMovement;
+//            //}
 
-            //anim.SetFloat(vertical, v, 0.1f, Time.deltaTime);
-            //anim.SetFloat(horizontal, v, 0.1f, Time.deltaTime);
-        }
+//            //anim.SetFloat(vertical, v, 0.1f, Time.deltaTime);
+//            //anim.SetFloat(horizontal, v, 0.1f, Time.deltaTime);
+//        }
 
-        public void CanRotate()
-        {
-            canRotate = true;
-        }
+//        public void CanRotate()
+//        {
+//            canRotate = true;
+//        }
 
-        public void StopRotation()
-        {
-            canRotate = false;
-        }
+//        public void StopRotation()
+//        {
+//            canRotate = false;
+//        }
 
-        public void EnableCombo()
-        {
-            //anim.SetBool("canDoCombo", true);
-        }
+//        public void EnableCombo()
+//        {
+//            //anim.SetBool("canDoCombo", true);
+//        }
 
-        public void DisableCombo()
-        {
-            //anim.SetBool("canDoCombo", false);
-        }
+//        public void DisableCombo()
+//        {
+//            //anim.SetBool("canDoCombo", false);
+//        }
 
-        private void OnAnimatorMove()
-        {
-            //if (playerManager.isInteracting == false)
-            //    return;
+//        private void OnAnimatorMove()
+//        {
+//            if (playerManager.isInteracting == false)
+//                return;
 
-            //float delta = Time.deltaTime;
-            //playerLocomotion.rigidbody.drag = 0;
-            //Vector3 deltaPosition = anim.deltaPosition;
-            //deltaPosition.y = 0;
-            //Vector3 velocity = deltaPosition / delta;
-            //playerLocomotion.rigidbody.velocity = velocity;
-        }
-    }
-}
+//            float delta = Time.deltaTime;
+//            playerLocomotion.rigidbody.drag = 0;
+//            Vector3 deltaPosition = anim.deltaPosition;
+//            deltaPosition.y = 0;
+//            Vector3 velocity = deltaPosition / delta;
+//            playerLocomotion.rigidbody.velocity = velocity;
+//        }
+//    }
+//}
--- run/Assets/Scripts/Player/PlayerLocomotion.cs
+++ run/Assets/Scripts/Player/PlayerLocomotion.cs
@@ -3,58 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //code revamped
-public class PlayerLocomotion : MonoBehaviour
+namespace Run
 {
-    InputManager inputManager;
-
-    Vector3 moveDirection; //direction of player movement
-    Transform cameraObject;
-    Rigidbody playerRigidbody;
-
-    public float movementSpeed = 7;
-    public float rotationSpeed = 15;
-
-    public void Awake()
+    public class PlayerLocomotion : MonoBehaviour
     {
-        inputManager = GetComponent<InputManager>(); //check for input manager comp
-        playerRigidbody = GetComponent<Rigidbody>(); //check for rb comp
-        cameraObject = Camera.main.transform; //scan for object tagged as main camera
-    }
+        InputManager inputManager;
 
-    public void HandleAllMovement()
-    {
-        HandleMovement();
-        HandleRotation();
-    }
+        Vector3 moveDirection; //direction of player movement
+        Transform cameraObject;
+        Rigidbody playerRigidbody;
 
-    private void HandleMovement()
-    {
-        moveDirection = cameraObject.forward * inputManager.verticalInput; //movement input in camera direction * vertical input
-        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput; //move left/right based on horizont input + camera direction
-        moveDirection.Normalize(); //keep vector direction, change length to 1
-        moveDirection.y = 0; //stop player from walking in air
-        moveDirection = moveDirection * movementSpeed;
+        public float movementSpeed = 7;
+        public float rotationSpeed = 15;
 
-        Vector3 movementVelocity = moveDirection; //store speed and direction info
-        playerRigidbody.velocity = movementVelocity; //move player based on calculations
-    }
+        public void Awake()
+        {
+            inputManager = GetComponent<InputManager>(); //check for input manager comp
+            playerRigidbody = GetComponent<Rigidbody>(); //check for rb comp
+            cameraObject = Camera.main.transform; //scan for object tagged as main camera
+        }
 
-    private void HandleRotation()
-    {
-        Vector3 targetDirection = Vector3.zero; //default to zero on all values
+        public void HandleAllMovement()
+        {
+            HandleMovement();
+            HandleRotation();
+        }
 
-        targetDirection = cameraObject.forward * inputManager.verticalInput; //set player to always facing forward/running direction
-        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
-        targetDirection.Normalize(); //search for direction of rotation based on input
-        targetDirection.y = 0;
+        private void HandleMovement()
+        {
+            moveDirection = cameraObject.forward * inputManager.verticalInput; //movement input in camera direction * vertical input
+            moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput; //move left/right based on horizont input + camera direction
+            moveDirection.Normalize(); //keep vector direction, change length to 1
+            moveDirection.y = 0; //stop player from walking in air
+            moveDirection = moveDirection * movementSpeed;
 
-        if (targetDirection == Vector3.zero)
-            targetDirection = transform.forward; // keep rotatio at looking position upon stopping
+            Vector3 movementVelocity = moveDirection; //store speed and direction info
+            playerRigidbody.velocity = movementVelocity; //move player based on calculations
+        }
 
-        Quaternion targetRotation = Quaternion.LookRotation(targetDirection); //quaternion = calculate rotation; look to target direction
-        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); //slerp = rotation between point A + B
+        private void HandleRotation()
+        {
+            Vector3 targetDirection = Vector3.zero; //default to zero on all values
 
-        transform.rotation = playerRotation; //set player rotation
+            targetDirection = cameraObject.forward * inputManager.verticalInput; //set player to always facing forward/running direction
+            targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+            targetDirection.Normalize(); //search for direction of rotation based on input
+            targetDirection.y = 0;
+
+            if (targetDirection == Vector3.zero)
+                targetDirection = transform.forward; // keep rotatio at looking position upon stopping
+
+            Quaternion targetRotation = Quaternion.LookRotation(targetDirection); //quaternion = calculate rotation; look to target direction
+            Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime); //slerp = rotation between point A + B
+
+            transform.rotation = playerRotation; //set player rotation
+        }
     }
 }
 
