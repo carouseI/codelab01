32d02b4f7c3e9dfb46bb21d83f0bd8d863544a19
headlesscrown <mdh8533@nyu.edu>
2022-03-28 14:29:54 -0400

    collision update

--- run/Assets/Scripts/BlockCharacterCollision.cs
+++ run/Assets/Scripts/BlockCharacterCollision.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class BlockCharacterCollision : MonoBehaviour
 {
+    public CapsuleCollider characterCollider;
+    public CapsuleCollider characterBlockerCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Physics.IgnoreCollision(characterCollider, characterBlockerCollider, true); //set collision to blockers
     }
 }
