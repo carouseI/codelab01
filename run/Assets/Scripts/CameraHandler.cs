a2298b45dfbc506d7b6af8a31b43ea3bfa8fc458
headlesscrown <mdh8533@nyu.edu>
2022-03-28 13:10:54 -0400

    parented player

--- run/Assets/Scripts/CameraHandler.cs
+++ run/Assets/Scripts/CameraHandler.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraHandler : MonoBehaviour
+namespace Run
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CameraHandler : MonoBehaviour
     {
-        
-    }
+        public Transform targetTransform; //camera target position
+        public Transform cameraTransform; //camera position
+        public Transform cameraPivotTransform; //rotation point
+        private Transform myTransform; //game object position
+        private Vector3 cameraTransformPosition; //vector position of camera
+        private LayerMask ignoreLayers;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public static CameraHandler singleton;
+
+        public float lookSpeed = 0.1f;
+        public float followSpeed = 0.1f;
+        public float pivotSpeed = 0.03f;
+
+        private float defaultPosition;
+        private float lookAngle;
+        private float pivotAngle;
+        public float minimumPivot = -35;
+        public float maximumPivot = 35;
+
+        private void Awake()
+        {
+            singleton = this;
+            myTransform = transform; //game object transform
+            defaultPosition = cameraTransform.localPosition.z;
+            ignoreLayers = ~(1 << 8 | 1 << 9 | 1 << 10);
+        }
     }
 }
