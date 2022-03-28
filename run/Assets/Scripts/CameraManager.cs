6fbc938dab51ba20534394ec9625ee4133ab9f8d
headlesscrown <mdh8533@nyu.edu>
2022-03-27 20:22:38 -0400

    camera manager update

--- run/Assets/Scripts/CameraManager.cs
+++ run/Assets/Scripts/CameraManager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform targetTransform; //object camera will follow, after every frame is processed
+    private Vector3 cameraFollowVelocity = Vector3.zero; //camera follow speed
+
+    public float cameraFollowSpeed = 0.2f; //float for smooth time
+
+    private void Awake()
     {
-        
+        targetTransform = FindObjectOfType<PlayerManager>().transform; //find player manager
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FollowTarget()
     {
-        
+        Vector3 targetPosition = Vector3.SmoothDamp
+            (transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed); //smoothDamp = move smth softly between 1 location to another, usually used for in-game camera; targetPos = current camera position, targetTransform = player position
+        transform.position = targetPosition; //set to new position
     }
 }
