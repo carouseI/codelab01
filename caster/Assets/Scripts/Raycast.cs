0f26ce57b7b8ee092581016c121d179485f610f7
headlesscrown <mdh8533@nyu.edu>
2022-04-16 17:22:33 -0400

    enemy layer added

--- caster/Assets/Scripts/Raycast.cs
+++ caster/Assets/Scripts/Raycast.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Raycast : MonoBehaviour
 {
+    [SerializeField]
+    LayerMask layerMask;
 
     // Update is called once per frame
     void Update()
     {
-        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, 20f)) //set raycast
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hitinfo, 20f, layerMask)) //set raycast
         {
             Debug.Log("Hit something"); //show msg when collision
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitinfo.distance, Color.red); //draw ray, ref when hit
