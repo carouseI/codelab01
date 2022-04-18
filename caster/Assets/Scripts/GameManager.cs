7eeae742ecb2accba9023e4742b2e64046f6d44d
headlesscrown <mdh8533@nyu.edu>
2022-04-18 01:31:32 -0400

    project duplicate

--- caster/Assets/Scripts/GameManager.cs
+++ caster/Assets/Scripts/GameManager.cs
@@ -5,70 +5,122 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject player;
-    public Text scoreText;
+    public GameObject player; //player
+    public Text scoreText; //score
+
     public Text highScoreText;
     public float highScore;
 
-    public GameObject prefab;
+    //public GameObject prefab;
 
-    public GameObject playersPlatform;
+    //public GameObject playersPlatform;
+
+    #region //mod 1
+    public RaycastHit hit; //hit
+    public GameObject enemy;
+
+    public bool hitOnce = false; //bool, update once
+
+    private int score;
+    private int UpdateScore;
+    Text text;
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        //level
-        for (int i = 0; i < 100; i++)
-        {
-            GameObject platform = Instantiate(prefab); // instantiate a new platform
-            platform.transform.position = new Vector3( // randomize its position
-                                           Random.Range(-9, 9),
-                                            i * 3,
-                                            transform.position.z
-                );
-            platform.transform.localScale = new Vector3( // randomize its scale
-                                           Random.Range(10, 30),
-                                            1.5f,
-                                            transform.localScale.z
-                );
-        }
+        text = GetComponent<Text>();
+        score = 0; //starting score
 
+        #region //level
+        //for (int i = 0; i < 100; i++)
+        //{
+        //    GameObject platform = Instantiate(prefab); // instantiate a new platform
+        //    platform.transform.position = new Vector3( // randomize its position
+        //                                   Random.Range(-9, 9),
+        //                                    i * 3,
+        //                                    transform.position.z
+        //        );
+        //    platform.transform.localScale = new Vector3( // randomize its scale
+        //                                   Random.Range(10, 30),
+        //                                    1.5f,
+        //                                    transform.localScale.z
+        //        );
+        //}
+        #endregion
+
+    }
+
+    public void ISCORE()
+    {
+        UpdateScore++;
+
+        Debug.Log("scored");
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = "Score:" + Mathf.Round((player.transform.position.y + 4.9f)); // display score according to its y position
-        highScoreText.text = "High Score:" + Mathf.Round(highScore); // display the high score
-        if (player.transform.position.y + 4.9f > highScore)
-        {
-            highScore = player.transform.position.y + 4.9f; // change the high score the current score is higher
-        }
+        #region //old
+        //scoreText.text = "Score:" + Mathf.Round((player.transform.position.y + 4.9f)); // display score according to its y position
+        //highScoreText.text = "High Score:" + Mathf.Round(highScore); // display the high score
+        //if (player.transform.position.y + 4.9f > highScore)
+        //{
+        //    highScore = player.transform.position.y + 4.9f; // change the high score the current score is higher
+        //}
 
-        //difficulities
+        ////difficulities
 
-        for (int j = 0; j < 10; j++)
+        //for (int j = 0; j < 10; j++)
+        //{
+        //    if (player.transform.position.y > j * 30)
+        //    {
+        //        playersPlatform.transform.localScale = new Vector3(15 - j, 1.5f, transform.localScale.z); // the platform will shrink as the player jump higher
+        //        //player.GetComponent<Player>().moveSpeed = 5 + (j / 2); // the frog will speed up as the player jump higher
+        //    }
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    Reset(); // press R to reset the frog's, the platform's and camera's position to starting point
+
+        //}
+        #endregion
+
+        #region //attempt 2
+        //Debug.DrawRay(transform.position, Vector3.down * 0.6f, Color.red);
+
+        //if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.6f))
+        //{
+        //    if (hit.collider.name == "Enemy" && !hitOnce)
+        //    {
+        //print("hit");
+        //Change 'hitOnce' to 'true' so that 'if' statement is not executed every frame while player is sitting on the Cube.'
+        //hitOnce = true;
+        //isAir = false;
+        //PlatformMovement.platformMovement.shouldMove = false;
+        //GameController.controller.score += 1; // i increase my score here
+        //        score = UpdateScore;
+        //    }
+        //}
+        //Change 'hitOnce' back to false when player moves away from Cube. That is 'RayCast' return false.
+        //else hitOnce = false;
+        #endregion
+
+        #region //attemp 3
+        if (Physics.Raycast(transform.position, Vector3.down * 0.6f))
         {
-            if (player.transform.position.y > j * 30)
+            if(hit.collider.name == "Enemy" && !hitOnce)
             {
-                playersPlatform.transform.localScale = new Vector3(15 - j, 1.5f, transform.localScale.z); // the platform will shrink as the player jump higher
-                //player.GetComponent<Player>().moveSpeed = 5 + (j / 2); // the frog will speed up as the player jump higher
+                print("hit");
+                hitOnce = true;
+
+                //score = UpdateScore;
+                score++;
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Reset(); // press R to reset the frog's, the platform's and camera's position to starting point
-
-        }
+        #endregion
 
     }
-
-    void Reset()
-    {
-        player.transform.position = new Vector3(0, -4.9f, 0);
-        playersPlatform.transform.position = new Vector3(-6.5f, -2.9f, 0);
-        Camera.main.transform.position = new Vector3(0, 0, -10);
-    }
-
 }
---
+++ caster/Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject score;
+
+    public RaycastHit hit; //Assign hit here so it doesn't get assigned every frame.
+
+    public bool hitOnce = false;    //Bool to update score once.
+
+    void Update()
+    {
+
+        Debug.DrawRay(transform.position, Vector3.down * 0.6f, Color.red);
+        //detect if player has hit the top of the platform
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.6f))
+        {
+            //i check if i hit the specific Cube and it has not been hit yet.
+            if (hit.collider.name == "Platform" && !hitOnce)
+            {
+                print("HIT!!!!!!!!!");
+                //Change 'hitOnce' to 'true' so that 'if' statement is not executed every frame while player is sitting on the Cube.'
+                hitOnce = true;
+                //isAir = false;
+                //PlatformMovement.platformMovement.shouldMove = false;
+                //player.score += 1; // i increase my score here
+            }
+        }
+        //Change 'hitOnce' back to false when player moves away from Cube. That is 'RayCast' return false.
+        else hitOnce = false;
+    }
+}
