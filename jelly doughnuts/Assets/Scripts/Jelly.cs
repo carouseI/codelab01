af5a905035e688a47d404a6b8f5a7b0468b18bb4
headlesscrown <mdh8533@nyu.edu>
2022-04-24 21:28:28 -0400

    set project to 3d

--- jelly doughnuts/Assets/Scripts/Jelly.cs
+++ jelly doughnuts/Assets/Scripts/Jelly.cs
@@ -8,7 +8,7 @@ public class Jelly : MonoBehaviour
     private int _amplitude = 1; //multiply Sine function by amplitude value
 
     [SerializeField]
-    private float _frequency = 0.1f;
+    private float _frequency = 0.1f; //adjust how quick movement is
 
     // Start is called before the first frame update
     void Start()
@@ -21,18 +21,20 @@ public class Jelly : MonoBehaviour
     {
         #region //Sin
         //float x = transform.position.x;
-        //float y = Mathf.Sin(Time.time * _frequency) * _amplitude; //set y to Sine wave; move through time, ping pong between 1 and -1
+        float y = Mathf.Sin(Time.time * _frequency) * _amplitude; //set y to Sine wave; move through time, ping pong between 1 and -1
         //float z = transform.position.z;
 
         //transform.position = new Vector3(x, y, z); //constantly set position to Vector3.0
         #endregion
 
         #region //Cos
-        float x = Mathf.Cos(Time.time *  _frequency) * _amplitude; //multiply to adjust height/length of cosine;  multiply by frequency to adjust how quick the movement is
-        float y = transform.position.y;
+        float x = Mathf.Cos(Time.time *  _frequency) * _amplitude; //multiply to adjust height/length of cosine
+        //float y = transform.position.y;
         float z = transform.position.z;
 
         transform.position = new Vector3(x, y, z); //set pos to Vector 3 @ 0
         #endregion
+
+        //sin + cos = perfect circles
     }
 }
