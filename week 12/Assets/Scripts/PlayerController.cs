5147640505a00df4fa247c551848c082c19a5ab6
headlesscrown <mdh8533@nyu.edu>
2022-04-25 11:49:44 -0400

    moved getInput to update

--- week 12/Assets/Scripts/PlayerController.cs
+++ week 12/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] //inspector visibility without publicising, private variable accessible via inspector
     private float moveSpeed = 2.0f;
 
+    Vector3 getInput;
+
+    private void Update()
+    {
+        getInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //getAxis H = W, S (up/down); getAxis V = A, D (left/right)
+        Debug.Log(getInput);
+    }
+
     private void FixedUpdate()
     {
-        Vector3 getInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //getAxis H = W, S (up/down); getAxis V = A, D (left/right)
-        Debug.Log(getInput);
         GetComponent<Rigidbody>().velocity = getInput * moveSpeed; //locate rb comp, set velocity
+
+        Vector3 lookPos = new Vector3(
+                transform.position.x + GetComponent<Rigidbody>().velocity.x,
+                transform.position.y,
+                transform.position.z + GetComponent<Rigidbody>().velocity.z
+            );
+        transform.LookAt(lookPos); //rotate object face to look at direction
     }
 }
