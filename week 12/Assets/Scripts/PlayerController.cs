ef91e351e6a2308852c0a5d52c5e643343e5a859
headlesscrown <mdh8533@nyu.edu>
2022-04-25 11:44:10 -0400

    [fix]controller update

--- week 12/Assets/Scripts/PlayerController.cs
+++ week 12/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //rec: use fixed update when using physics/moving things
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] //inspector visibility without publicising, private variable accessible via inspector
+    private float moveSpeed = 2.0f;
+
+    private void FixedUpdate()
     {
-        
+        Vector3 getInput = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")); //getAxis H = W, S (up/down); getAxis V = A, D (left/right)
+        Debug.Log(getInput);
+        GetComponent<Rigidbody>().velocity = getInput * moveSpeed; //locate rb comp, set velocity
     }
 }
