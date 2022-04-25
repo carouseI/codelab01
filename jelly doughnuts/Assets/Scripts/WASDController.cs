604ff05c794f76131022fc1378f897f95694f3eb
headlesscrown <mdh8533@nyu.edu>
2022-04-24 22:35:51 -0400

    update

---
+++ jelly doughnuts/Assets/Scripts/WASDController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WASDController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+
+        }
+
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
+
+        }
+    }
+}
