75afc52bc4384f45e6b7ec61a00e6645df561ed2
headlesscrown <mdh8533@nyu.edu>
2022-04-24 23:04:39 -0400

    script reset

--- jelly doughnuts/Assets/Scripts/Score.cs
+++ jelly doughnuts/Assets/Scripts/Score.cs
@@ -5,24 +5,6 @@ using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
-    private Text score;
-    private int scoreAmount;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        scoreAmount = 0; //set default
-        score = GetComponent<Text>(); //get comp
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        score.text = scoreAmount.ToString();
-    }
-
-    public void AddScore()
-    {
-        scoreAmount += 10; //increment by 10
-    }
+    
+    
 }
