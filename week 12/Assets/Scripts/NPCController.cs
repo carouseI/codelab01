77ad14dac6efeeeb84a3b22e1369538f945df50b
headlesscrown <mdh8533@nyu.edu>
2022-04-25 12:06:58 -0400

    util script added

---
+++ week 12/Assets/Scripts/NPCController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util; //direct access to public static content in util script without references
+
+public class NPCController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
