fbfcd8388903b5e1b6636b6526e863dff52bada2
headlesscrown <mdh8533@nyu.edu>
2022-04-24 22:57:31 -0400

    update

--- jelly doughnuts/Assets/Scripts/Score.cs
+++ jelly doughnuts/Assets/Scripts/Score.cs
@@ -16,7 +16,7 @@ public class Score : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         score.text = scoreAmount.ToString();
     }
