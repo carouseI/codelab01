97e9860f8dc8d2090f7e581561b4ef40a571a47b
headlesscrown <mdh8533@nyu.edu>
2022-04-24 21:22:29 -0400

    cosine added

--- jelly doughnuts/Assets/Scripts/Jelly.cs
+++ jelly doughnuts/Assets/Scripts/Jelly.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Jelly : MonoBehaviour
 {
+    [SerializeField]
+    private int _amplitude = 1; //multiply Sine function by amplitude value
+
+    [SerializeField]
+    private float _frequency = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +19,20 @@ public class Jelly : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = transform.position.x;
-        float y = Mathf.Sin(Time.time); //set y to Sine wave; move through time, ping pong between 1 and -1
+        #region //Sin
+        //float x = transform.position.x;
+        //float y = Mathf.Sin(Time.time * _frequency) * _amplitude; //set y to Sine wave; move through time, ping pong between 1 and -1
+        //float z = transform.position.z;
+
+        //transform.position = new Vector3(x, y, z); //constantly set position to Vector3.0
+        #endregion
+
+        #region //Cos
+        float x = Mathf.Cos(Time.time *  _frequency) * _amplitude; //multiply to adjust height/length of cosine;  multiply by frequency to adjust how quick the movement is
+        float y = transform.position.y;
         float z = transform.position.z;
 
-        transform.position = new Vector3(x, y, z); //constantly set position to Vector3.0
+        transform.position = new Vector3(x, y, z); //set pos to Vector 3 @ 0
+        #endregion
     }
 }
