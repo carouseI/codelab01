1dc1283a195a28ac57852b0a002c6f2a8d2fd1ad
headlesscrown <mdh8533@nyu.edu>
2022-05-09 16:19:25 -0400

    inventory setup

--- mul/Assets/Scripts/InventoryUI.cs
+++ mul/Assets/Scripts/InventoryUI.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
-public class InventoryUI : MonoBehaviour
+namespace Mul
 {
-    // Start is called before the first frame update
-    void Start()
+    public class InventoryUI : MonoBehaviour
     {
-        
-    }
+        private TextMeshProUGUI crystalText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Start is called before the first frame update
+        void Start()
+        {
+            crystalText = GetComponent<TextMeshProUGUI>(); //get tmp comp
+        }
+
+        public void UpdateCrystalText(PlayerInventory playerInventory) //use playerInvetory as parameter
+        {
+            crystalText.text = playerInventory.CrystalCount.ToString(); //set to inventory count
+        }
     }
 }
--- mul/Assets/Scripts/PlayerInventory.cs
+++ mul/Assets/Scripts/PlayerInventory.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Mul
 {
@@ -8,10 +9,12 @@ namespace Mul
     {
         public int CrystalCount { get; private set; } //can be read by other scripts, can only be set in this one
 
+        public UnityEvent<PlayerInventory> OnCrystalCollected; //set event, will take argument of type playerInventory
+
         public void CrystalsCollected()
         {
             CrystalCount++; //increment
-
+            OnCrystalCollected.Invoke(this); //invoke event, pass through playerInventory to subcribers
         }
     }
 }
