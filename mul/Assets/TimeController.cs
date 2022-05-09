23553cf510a675fbf6dea3a920e4bf86d4822235
headlesscrown <mdh8533@nyu.edu>
2022-05-09 17:07:47 -0400

    dayNight cycle script update

--- mul/Assets/TimeController.cs
+++ mul/Assets/TimeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,18 +17,28 @@ namespace Mul
         [SerializeField]
         private TextMeshProUGUI timeText; //display current time
 
-        //track current time; frame by frame progression
+        private DateTime currentTime; //track current time; frame by frame progression
 
         // Start is called before the first frame update
         void Start()
         {
-
+            currentTime = DateTime.Now.Date + TimeSpan.FromHours(startHour); //use dateTime comp, add start time; use timeSpan hrs from method
         }
 
         // Update is called once per frame
         void Update()
         {
+            UpdateTimeOfDay(); //call method
+        }
 
+        private void UpdateTimeOfDay()
+        {
+            currentTime = currentTime.AddSeconds(Time.deltaTime * timeMultiplier); //update time
+
+            if(timeText != null) //if not null
+            {
+                timeText.text = currentTime.ToString("HH:mm"); //set text to current time, display in 24hr format
+            }
         }
     }
 }
