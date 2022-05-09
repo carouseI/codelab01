7304785c83d44e8c7a8f74e5188b4652c32bf190
headlesscrown <mdh8533@nyu.edu>
2022-05-09 19:00:52 -0400

    camera update

--- mul/Assets/Scripts/CameraTransform.cs
+++ mul/Assets/Scripts/CameraTransform.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CameraTransform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = player.transform.position; //set camera position to player position
     }
 }
