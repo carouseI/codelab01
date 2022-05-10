8ee8d3a59ca8ea585cdef440daf806b59e9856cf
headlesscrown <mdh8533@nyu.edu>
2022-05-09 22:08:07 -0400

    inventory update

--- pale blue./Assets/Scripts/InventoryUI.cs
+++ pale blue./Assets/Scripts/InventoryUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 namespace PaleBlue
 {
@@ -9,15 +10,27 @@ namespace PaleBlue
     {
         private TextMeshProUGUI crystalText;
 
+        private int crystalTotal;
+
+        [SerializeField]
+        private int maxCollectibles;
+
         // Start is called before the first frame update
         void Start()
         {
             crystalText = GetComponent<TextMeshProUGUI>(); //get tmp comp
+            crystalTotal = 0; //set inventory start count
         }
 
         public void UpdateCrystalText(PlayerInventory playerInventory) //use playerInvetory as parameter
         {
             crystalText.text = playerInventory.CrystalCount.ToString(); //set to inventory count
+            crystalTotal++; //increment
+
+            if(crystalTotal >= maxCollectibles) //if max is reached
+            {
+                SceneManager.LoadScene("End Menu"); //go to scene
+            }
         }
     }
 }
