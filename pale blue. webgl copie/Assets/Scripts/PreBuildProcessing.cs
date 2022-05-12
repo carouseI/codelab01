29e806695a651e2f62409ca322a7f22cd22d29eb
headlesscrown <mdh8533@nyu.edu>
2022-05-12 14:27:38 -0400

    webgl test, pre-build script added

---
+++ pale blue. webgl copie/Assets/Scripts/PreBuildProcessing.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+
+public class PreBuildProcessing : IPreprocessBuildWithReport
+{
+    public int callbackOrder => 1;
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        System.Environment.SetEnvironmentVariable("EMSDK_PYTHON", "/Library/Frameworks/Python.framework/Versions/2.7/bin/python");
+    }
+}
